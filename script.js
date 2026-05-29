6fa9a099a38254844d39a87081b122a029a1a56e
shakoorabdul <mashwani2000@hotmail.com>
2026-05-28 22:56:23 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+var studentName = "Abdul Shakoor";
+var studentAge = 35;
+var isEnroled = true;
+console.log("Student's name is " + studentName, "and Student's Age is " + studentAge + " and enroled is " + isEnroled );
+// String value
+var testMessage = "This is a text message";
+console.log(testMessage);
+// Number value
+var integer = 55;
+console.log(integer);
+//Boolean
+var condition = true;
+console.log(condition);
+// Array
+var fruits = ["apple","banana","cherry"];
+console.log(fruits + " are exmapoles of fruits");
+// Object
+var info = {firstname : "Abdul", lastName : "Shakoor", age : 37};
+console.log("Hi, my name is " + info.firstname +" "+info.lastName +" and my age is "+info.age);
+// Basic operators
+var numberOne = 20;
+var numberTwo = 5;
+console.log("Subtraction:" , numberOne - numberTwo);
+console.log("Multiplication: " ,numberOne * numberTwo);
+console.log("Division: ", numberOne / numberTwo);
+console.log("Greagter then : ",numberOne > numberTwo);
+console.log("Less then: ",numberOne < numberTwo);
+console.log("Strictly equaly to:", numberOne === numberTwo);
+console.log("Not equal to : ",numberOne !== numberTwo);
+console.log("And:", numberOne && numberTwo);
+console.log("Or: ",numberOne || numberTwo);
+console.log("Not equali to: ",numberOne != numberTwo);
